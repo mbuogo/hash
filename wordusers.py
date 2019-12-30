478ba5725ad37817706a180665978c0f42dbe5f3
Mateus Buogo <41753941+mbuogo@users.noreply.github.com>
2019-12-30 10:22:00 -0300

    Update wordusers.py

--- wordusers.py
+++ wordusers.py
@@ -1,1 +1,13 @@
 import sys
+import string
+
+wordlist = open('wordlist-users.txt', 'w')
+
+
+if len(sys.argv) < 2:
+    print("\n Exemplo: python "+ sys.argv[0] + " wordlistusuarios.txt \n")
+    sys.exit(0)
+else:
+  with open(sys.argv[1]) as file:
+    for line in file:
+      
