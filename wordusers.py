98b1a36d7e2559fcb347bd16120aeb9fbc81d107
Mateus Buogo <41753941+mbuogo@users.noreply.github.com>
2019-12-30 11:01:58 -0300

    Update wordusers.py

--- wordusers.py
+++ wordusers.py
@@ -3,11 +3,16 @@ import string
 
 wordlist = open('wordlist-users.txt', 'w')
 
-
 if len(sys.argv) < 2:
     print("\n Exemplo: python "+ sys.argv[0] + " wordlistusuarios.txt \n")
     sys.exit(0)
 else:
   with open(sys.argv[1]) as file:
     for line in file:
-      print (line)
+      line=line.rstrip('\n')
+      print(line)
+      print(line.replace(' ', '.'))
+      print(line.replace(' ', ''))
+      a=line.replace(' ', '')+'01\n'
+      wordlist.write(a)
+  wordlist.close()
